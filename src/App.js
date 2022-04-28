9e904a4862c3994209fab6a9f366ad4680ca86ae
Krish <krishshah235@gmail.com>
2022-04-28 18:06:44 +0530

    static app react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <div className="App">
-     <h1>KRISH PIZZA</h1>
+    <Navbar/>
+    <Homescreen/>
     </div>
   );
 }
---
+++ src/pizzasdata.js
@@ -0,0 +1,79 @@
+const pizzas =[{
+    "name": "PEPPER BARBECUE ",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 450
+
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Peper barbeque"
+},
+{
+    "name": "PEPPER BARBECUE 2 ",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 450
+
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Peper barbeque 2"
+},
+{
+    "name": "PEPPER BARBECUE 3 ",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 450
+
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Peper barbeque 3"
+},
+{
+    "name": "PEPPER BARBECUE 4 ",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 450
+
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description": "Peper barbeque 4"
+}];
+
+export default pizzas
+
